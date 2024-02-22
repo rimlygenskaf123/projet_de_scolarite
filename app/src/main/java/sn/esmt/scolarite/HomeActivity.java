c0a235b13e42425b98a77e74b5dc52a00f55635f
Rimbe KAFANDO <rimgenspirikaf@gmail.com>
2024-02-22 11:29:53 +0100

    Livraison de projet Android

---
+++ app/src/main/java/sn/esmt/scolarite/HomeActivity.java
@@ -0,0 +1,14 @@
+package sn.esmt.scolarite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
